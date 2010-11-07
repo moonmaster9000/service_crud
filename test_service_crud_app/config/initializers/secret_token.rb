14628970f557b70ef9729e9ba4ea29c3f6cee741
Matt Parker <moonmaster9000@gmail.com>
2010-11-07 13:09:46 -0500

    Version bump to 0.0.0

---
+++ test_service_crud_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestServiceCrudApp::Application.config.secret_token = '6e456350c682b9f55ad738c03b7574754386add480bf8220f1ee58b3ca86c1adc90385d37444f27a12ebfbae05e33d7d3c0d9bb4842628b213c7079a0f338747'
